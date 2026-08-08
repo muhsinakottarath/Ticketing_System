ca202d1df0727876e950197988f9c41e0b64ae81
kottarathmuhsina@gmail.com <kottarathmuhsina@gmail.com>
2026-08-08 08:57:44 +0000

    initial commit
    updated sql

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS tickets(
+    ticket_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title STRING NOT NULL,
+    status STRING NOT NULL,
+    created_by STRING NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS message(
+    message_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    ticket_id BIGINT NOT NULL,
+    description STRING NOT NULL,
+    author STRING NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (ticket_id) REFERENCES tickets(ticket_id)
+);
+
+
+
